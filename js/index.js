983bbea9576c68a4dbed9a300fabcffc769ac679
milenaal17 <milenaal17@gmail.com>
2022-08-18 11:39:48 -0300

    redir

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
+    window.location = "login.html"
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
